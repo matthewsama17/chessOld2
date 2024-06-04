79828ccbca9050e3c7d7fde6d40132d263e1cf7c
Matthew Herron <m.3herron@gmail.com>
2024-06-03 21:28:21 -0600

    Implemented MemoryAuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,7 +3,7 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
-    void createAuth(String username);
+    String createAuth(String username);
     AuthData getAuth(String authToken);
     void deleteAuth(String authToken);
     void clear();
---
+++ server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
@@ -0,0 +1,42 @@
+package dataaccess.memory;
+
+import dataaccess.AuthDAO;
+import model.AuthData;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class MemoryAuthDAO implements AuthDAO {
+
+    private static ArrayList<AuthData> auths = new ArrayList<>();
+
+    @Override
+    public String createAuth(String username) {
+        String authToken = UUID.randomUUID().toString();
+        auths.add(new AuthData(authToken,username));
+        return authToken;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        for(AuthData auth : auths) {
+            if(auth.authToken() == authToken) {
+                return auth;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        for(AuthData auth : auths) {
+            if(auth.authToken() == authToken) {
+                auths.remove(auth);
+            }
+        }
+    }
+
+    @Override
+    public void clear() {
+        auths.clear();
+    }
+}
