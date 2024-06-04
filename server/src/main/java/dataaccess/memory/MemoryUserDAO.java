da902de125d36ab07da4980f0cd25b0f8a7c6252
Matthew Herron <m.3herron@gmail.com>
2024-06-03 21:17:23 -0600

    Implemented MemoryUserDAO

---
+++ server/src/main/java/dataaccess/memory/MemoryUserDAO.java
@@ -0,0 +1,30 @@
+package dataaccess.memory;
+
+import dataaccess.UserDAO;
+import model.UserData;
+import java.util.ArrayList;
+
+public class MemoryUserDAO implements UserDAO {
+
+    private static ArrayList<UserData> users = new ArrayList<>();
+
+    @Override
+    public void createUser(String username, String password, String email) {
+        users.add(new UserData(username, password, email));
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        for(UserData user : users) {
+            if(user.username() == username) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        users = new ArrayList<UserData>();
+    }
+}
