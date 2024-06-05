3bf7993a90df51f1e3d813c7ae3e0d6e99cdf74e
Matthew Herron <m.3herron@gmail.com>
2024-06-04 20:30:22 -0600

    Implemented ListGamesService.

--- server/src/main/java/service/services/ListGamesService.java
+++ server/src/main/java/service/services/ListGamesService.java
@@ -1,10 +1,28 @@
 package service.services;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
 import service.request.ListGamesRequest;
 import service.result.ListGamesResult;
 
 public class ListGamesService {
     public static ListGamesResult listGames(ListGamesRequest listGamesRequest) {
-        return null;
+        String authToken = listGamesRequest.authToken();
+
+        ListGamesResult result = new ListGamesResult();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
+        if(authDAO.getAuth(authToken) == null) {
+            result.setCode(401);
+            result.setError("Error: unauthorized");
+            return result;
+        }
+
+        result.setCode(200);
+        result.setGames(gameDAO.listGames());
+        return result;
     }
 }
