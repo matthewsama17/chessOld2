4b0056c4cd8cd66667a52df1071fb386242ca832
Matthew Herron <m.3herron@gmail.com>
2024-06-04 19:53:41 -0600

    Implemented ClearService.

--- server/src/main/java/service/services/ClearService.java
+++ server/src/main/java/service/services/ClearService.java
@@ -1,9 +1,26 @@
 package service.services;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import dataaccess.memory.MemoryUserDAO;
 import service.result.Result;
 
 public class ClearService {
     public static Result clear() {
-        return null;
+        Result result = new Result();
+
+        AuthDAO authDAO = new MemoryAuthDAO();
+        authDAO.clear();
+
+        GameDAO gameDAO = new MemoryGameDAO();
+        gameDAO.clear();
+
+        UserDAO userDAO = new MemoryUserDAO();
+        userDAO.clear();
+
+        return result;
     }
 }
