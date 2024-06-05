adf19380837287dcfc4b2ed1a79124ee2023bbf0
Matthew Herron <m.3herron@gmail.com>
2024-06-04 20:23:28 -0600

    Implemented LogoutService.

--- server/src/main/java/service/services/LogoutService.java
+++ server/src/main/java/service/services/LogoutService.java
@@ -1,10 +1,26 @@
 package service.services;
 
+import dataaccess.AuthDAO;
+import dataaccess.memory.MemoryAuthDAO;
 import service.request.LogoutRequest;
 import service.result.Result;
 
 public class LogoutService {
     public static Result logout(LogoutRequest logoutRequest) {
-        return null;
+        String authToken = logoutRequest.authToken();
+
+        Result result = new Result();
+        AuthDAO authDAO = new MemoryAuthDAO();
+
+        if(authDAO.getAuth(authToken) == null) {
+            result.setCode(401);
+            result.setError("Error: unauthorized");
+            return result;
+        }
+
+        authDAO.deleteAuth(authToken);
+
+        result.setCode(200);
+        return result;
     }
 }
