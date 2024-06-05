415014c89092b574fa125749d692c8fd69bf23bb
Matthew Herron <m.3herron@gmail.com>
2024-06-04 21:07:22 -0600

    Implemented JoinGameService.

--- server/src/main/java/service/services/JoinGameService.java
+++ server/src/main/java/service/services/JoinGameService.java
@@ -1,10 +1,70 @@
 package service.services;
 
+import chess.ChessGame;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import model.AuthData;
+import model.GameData;
 import service.request.JoinGameRequest;
 import service.result.Result;
 
 public class JoinGameService {
     public static Result joinGame(JoinGameRequest joinGameRequest) {
-        return null;
+        String authToken = joinGameRequest.authToken();
+        ChessGame.TeamColor playerColor = joinGameRequest.playerColor();
+        int gameID = joinGameRequest.gameID();
+
+        Result result = new Result();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
+        AuthData authData = authDAO.getAuth(authToken);
+        if(authData == null) {
+            result.setCode(401);
+            result.setError("Error: unauthorized");
+            return result;
+        }
+        String username = authData.username();
+
+        GameData gameData = gameDAO.getGame(gameID);
+        if(gameData == null || playerColor == null) {
+            result.setCode(400);
+            result.setError("Error: bad request");
+            return result;
+        }
+
+
+        if(((playerColor == ChessGame.TeamColor.BLACK) && (gameData.blackUsername() != null))
+                || ((playerColor == ChessGame.TeamColor.WHITE) && (gameData.whiteUsername() != null))) {
+            result.setCode(403);
+            result.setError("Error: already taken");
+            return result;
+        }
+
+        String whiteUsername = gameData.whiteUsername();
+        String blackUsername = gameData.blackUsername();
+        if(playerColor == ChessGame.TeamColor.WHITE) {
+            whiteUsername = username;
+        }
+        else if(playerColor == ChessGame.TeamColor.BLACK) {
+            blackUsername = username;
+        }
+        String gameName = gameData.gameName();
+        ChessGame game = gameData.game();
+        GameData newGameData = new GameData(gameID,whiteUsername,blackUsername,gameName,game);
+
+        try {
+            gameDAO.updateGame(gameID, newGameData);
+        } catch(DataAccessException ex) {
+            result.setCode(400);
+            result.setError("Error: bad request");
+            return result;
+        }
+
+        result.setCode(200);
+        return result;
     }
 }
