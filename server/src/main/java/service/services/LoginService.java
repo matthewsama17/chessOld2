895aff2d97b5a690df96d7c880779c69fa8c458a
Matthew Herron <m.3herron@gmail.com>
2024-06-04 20:16:47 -0600

    Implemented LoginService.

--- server/src/main/java/service/services/LoginService.java
+++ server/src/main/java/service/services/LoginService.java
@@ -1,14 +1,34 @@
 package service.services;
 
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryUserDAO;
+import model.UserData;
 import service.request.LoginRequest;
 import service.result.LoginResult;
 
 public class LoginService {
     public static LoginResult login(LoginRequest loginRequest) {
+        String username = loginRequest.username();
+        String password = loginRequest.password();
+
         LoginResult result = new LoginResult();
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
 
+        UserData userData = userDAO.getUser(username);
 
+        if(userData.password() != password) {
+            result.setCode(401);
+            result.setError("Error: unauthorized");
+            return result;
+        }
 
+        String authToken = authDAO.createAuth(username);
+        result.setCode(200);
+        result.setAuthToken(authToken);
+        result.setUsername(username);
         return result;
     }
 }
