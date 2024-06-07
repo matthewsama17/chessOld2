2da2bf3b9bf824b57d451a4d423824eb9b364204
Matthew Herron <m.3herron@gmail.com>
2024-06-06 21:28:51 -0600

    Implemented Handler.register().

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -1,7 +1,9 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
 import service.*;
+import request.*;
 import result.*;
 import spark.Request;
 import spark.Response;
@@ -24,7 +26,30 @@ public class Handler {
     }
 
     public static String register(Request req, Response res) {
-        return "creates a new user";
+        RegisterRequest registerRequest = gson.fromJson(req.body(), RegisterRequest.class);
+        LoginResult loginResult;
+
+        try {
+            loginResult = RegisterService.register(registerRequest);
+        } catch(DataAccessException ex) {
+            if(ex.equals(new DataAccessException("Error: already taken"))) {
+                res.status(403);
+                ErrorMessage errorMessage = new ErrorMessage("Error: already taken");
+                return gson.toJson(errorMessage);
+            }
+            else {
+                res.status(400);
+                ErrorMessage errorMessage = new ErrorMessage("Error: bad request");
+                return gson.toJson(errorMessage);
+            }
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return gson.toJson(loginResult);
     }
 
     public static String login(Request req, Response res) {
