061e820672b0d6d4f50e999e531cff5d90c2d86e
Matthew Herron <m.3herron@gmail.com>
2024-06-06 21:44:28 -0600

    Implemented the remaining methods of Handler.

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -53,22 +53,119 @@ public class Handler {
     }
 
     public static String login(Request req, Response res) {
-        return "begins a session";
+        LoginRequest loginRequest = gson.fromJson(req.body(), LoginRequest.class);
+        LoginResult loginResult;
+
+        try {
+            loginResult = LoginService.login(loginRequest);
+        } catch(DataAccessException ex) {
+            res.status(401);
+            ErrorMessage errorMessage = new ErrorMessage("Error: unauthorized");
+            return gson.toJson(errorMessage);
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return gson.toJson(loginResult);
     }
 
     public static String logout(Request req, Response res) {
-        return "ends a session";
+        LogoutRequest logoutRequest = gson.fromJson(req.body(), LogoutRequest.class);
+
+        try {
+            LogoutService.logout(logoutRequest);
+        } catch(DataAccessException ex) {
+            res.status(401);
+            ErrorMessage errorMessage = new ErrorMessage("Error: unauthorized");
+            return gson.toJson(errorMessage);
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return "{}";
     }
 
     public static String listGames(Request req, Response res) {
-        return "returns a list of all the games";
+        ListGamesRequest listGamesRequest = gson.fromJson(req.body(), ListGamesRequest.class);
+        ListGamesResult listGamesResult;
+
+        try {
+            listGamesResult = ListGamesService.listGames(listGamesRequest);
+        } catch(DataAccessException ex) {
+            res.status(401);
+            ErrorMessage errorMessage = new ErrorMessage("Error: unauthorized");
+            return gson.toJson(errorMessage);
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return gson.toJson(listGamesResult);
     }
 
     public static String createGame(Request req, Response res) {
-        return "creates a new game";
+        CreateGameRequest createGameRequest = gson.fromJson(req.body(), CreateGameRequest.class);
+        CreateGameResult createGameResult;
+
+        try {
+            createGameResult = CreateGameService.createGame(createGameRequest);
+        } catch(DataAccessException ex) {
+            if(ex.equals(new DataAccessException("Error: unauthorized"))) {
+                res.status(401);
+                ErrorMessage errorMessage = new ErrorMessage("Error: unauthorized");
+                return gson.toJson(errorMessage);
+            }
+            else {
+                res.status(400);
+                ErrorMessage errorMessage = new ErrorMessage("Error: bad request");
+                return gson.toJson(errorMessage);
+            }
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return gson.toJson(createGameResult);
     }
 
     public static String joinGame(Request req, Response res) {
-        return "adds the user as a player in a game";
+        JoinGameRequest joinGameRequest = gson.fromJson(req.body(), JoinGameRequest.class);
+
+        try {
+            JoinGameService.joinGame(joinGameRequest);
+        } catch(DataAccessException ex) {
+            if(ex.equals(new DataAccessException("Error: unauthorized"))) {
+                res.status(401);
+                ErrorMessage errorMessage = new ErrorMessage("Error: unauthorized");
+                return gson.toJson(errorMessage);
+            }
+            else if(ex.equals(new DataAccessException("Error: already taken"))) {
+                res.status(403);
+                ErrorMessage errorMessage = new ErrorMessage("Error: already taken");
+                return gson.toJson(errorMessage);
+            }
+            else {
+                res.status(400);
+                ErrorMessage errorMessage = new ErrorMessage("Error: bad request");
+                return gson.toJson(errorMessage);
+            }
+        } catch(Error error) {
+            res.status(500);
+            ErrorMessage errorMessage = new ErrorMessage("Error: " + error.getMessage());
+            return gson.toJson(errorMessage);
+        }
+
+        res.status(200);
+        return "{}";
     }
 }
